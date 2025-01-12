6b0f38d89bfbc0efd5bec7fd82a2b23367b99f80
gleni1 <glenipoleshi@gmail.com>
2025-01-11 23:14:04 -0500

    Working on functions to build the AST

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 )
 
 type token struct {
-  kind  string 
-  value string 
+  kind    string 
+  value   string 
 }
+
+
 
 func tokenizer(input string) []token {
   input += "\n"
@@ -19,54 +21,50 @@ func tokenizer(input string) []token {
   tokens := []token{}
 
   for current < len([]rune(input)) {
+
     char := string([]rune(input)[current])
 
     if char == "(" {
       tokens = append(tokens, token{
-        kind: "paren",
+        kind:   "paren",
         value:  "(",
       })
 
       current++
-
       continue
     }
 
     if char == ")" {
       tokens = append(tokens, token{
-        kind: "paren",
+        kind:   "paren",
         value:  ")",
       })
-      current ++
-      continue
-    }
 
-
-    if char == " " {
       current++
       continue
     }
 
+    if char == " " {
+      current++ 
+      continue
+    }
 
     if isNumber(char) {
       value := ""
 
-      
       for isNumber(char) {
         value += char 
-        current++ 
+        current++
         char = string([]rune(input)[current])
       }
 
       tokens = append(tokens, token{
-        kind: "number",
-        value: value,
+        kind:   "number",
+        value:  value,
       })
 
       continue
-
     }
-
 
     if isLetter(char) {
       value := ""
@@ -78,63 +76,114 @@ func tokenizer(input string) []token {
       }
 
       tokens = append(tokens, token{
-        kind: "name",
-        value: value,
+        kind:   "name",
+        value:  value,
       })
       continue
     }
     break
   }
+
   return tokens
 }
 
-
-// isNumber accepts a string and will check to see whether or not what has been
-// passed through is between the range of 0 - 9.
 func isNumber(char string) bool {
-	if char == "" {
-		return false
-	}
-	n := []rune(char)[0]
-	if n >= '0' && n <= '9' {
-		return true
-	}
-	return false
+  if char == "" {
+    return false
+  }
+
+  n := []rune(char)[0]
+  if n >= '0' && n <= '9' {
+    return true
+  }
+  return false
 }
 
-// isLetter works in a similar way to isNumber, but checks the range for a
-// letter in the range of a - z.
-func isLetter(char string) bool {
-	if char == "" {
-		return false
-	}
-	n := []rune(char)[0]
-	if n >= 'a' && n <= 'z' {
-		return true
-	}
-	return false
+func isLetter() {
+  if char == "" {
+    return false
+  }
+
+  n := []rune(char)[0]
+  if n >= 'a' && n <= 'z' {
+    return true
+  }
+
+  return false
+}
+
+type node struct {
+  kind        string  
+  value       string  
+  name        string   
+  callee      *node 
+  expression  *node 
+  body        []node 
+  params      []node 
+  arguments   *[]node 
+  context     *[]node
 }
 
 
- type node struct {
-	kind       string
-	value      string
-	name       string
-	callee     *node
-	expression *node
-	body       []node
-	params     []node
-	arguments  *[]node
-	context    *[]node
+type ast node 
+
+var pc int 
+
+var pt []token 
+
+func parser(tokens []token) ast {
+  pc = 0 
+  pt = tokens 
+
+  ast := ast{
+    kind:   "Program",
+    body:   []node{},
+  }
+
+  for pc < len(pt) {
+    ast.body = append(ast.body, walk())
+  }
+
+  return ast
 }
 
-// Type `ast` is just another alias type. I find this makes part of the code
-// more readable, as you'll come to see that there are a ton of references to
-// `node`.
-type ast node
 
-// This is the counter variable that we'll use for parsing.
-var pc int
+func walk() node {
+  token := pt[pc]
 
-// This variable will store our slice of `token`s inside of it.
-var pt []token
+  if token.kind == "number" {
+    pc++
+
+    return node{
+      kind: "NumberLiteral",
+      value: token.value,
+    }
+  }
+
+
+  if token.kind == "paren" && token.value== "(" {
+    pc++
+    token = pt[pc]
+
+    n := node {
+      kind:     "CallExpression",
+      name:     token.value, 
+      params:   []node{},
+    }
+
+    pc++ 
+    token = pt[pc]
+
+    for token.kind != "paren" || (token.kind == "paren" && token.value != ")") {
+      n.params = append(n.params, walk())
+      token = pt[pc]
+    }
+
+    pc++ 
+
+    return n
+  }
+
+  log.Fatal(token.kind)
+  return node{}
+}
